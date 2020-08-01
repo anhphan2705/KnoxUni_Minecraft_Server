ea28383fe0fcb50029e3c2298b49b801369753cf
anhphan2705 <anh.phan@students.htes.org>
2020-08-01 14:06:27 -0400

    First Launch Update

--- Main/start.bat
+++ Main/start.bat
@@ -1,3 +1,4 @@
 @echo off
-java -Dfile.encoding=UTF-8 -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+DisableExplicitGC -Xms2048M -Xmx2048M -jar Spigot.jar nogui
+java -Dfile.encoding=UTF-8 -XX:+UseConcMarkSweepGC -XX:+DisableExplicitGC -Xms2048M -Xmx2048M -jar Spigot.jar nogui
+pause
 done
